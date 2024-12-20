f1330e0c2763e3f3983fc72d78a5b80f34aa9580
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-12-20 12:09:21 +0000

    fix

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -4,7 +4,7 @@ import OpenAI from 'openai'
 import path from 'path'
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 })
 
 const transcribe = async (audioPath: string, language: string) => {
